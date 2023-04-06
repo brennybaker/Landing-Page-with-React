f213631f4db1fd05f9f8c9d365bfcb5598af7551
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-04-06 14:38:48 +0000

    Initial changes with card, jumbotron, navbar, trying to import to home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,8 +3,9 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
+const Home =() => {
 	return (
 		<div className="text-center">
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
@@ -21,6 +22,6 @@ const Home = () => {
 			</p>
 		</div>
 	);
-};
+}
 
 export default Home;
