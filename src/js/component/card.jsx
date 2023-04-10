a1c966bf680e0cefc0924222a72f9ef2bce05251
BreAnna Baker <124074031+brennybaker@users.noreply.github.com>
2023-04-10 00:38:08 +0000

    Fixed button text/Final

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -9,7 +9,7 @@ const Card = (props) => {
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
                         <p className="card-text">{props.text}</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href="#" className="btn btn-primary">Find Out More!</a>
                     </div>
             </div>
         </div> 
